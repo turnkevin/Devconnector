57e4924940851a2fe3054f88a710be3dcbaf64e0
turnkevin <lushikai277@gmail.com>
2022-02-05 22:47:10 +1100

    string

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,7 +14,6 @@ router.post(
   [auth, check("text", "Text is  required").not().isEmpty()],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
